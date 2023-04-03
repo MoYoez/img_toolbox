93b5366e0a7aa2e6e3f86854e20707c9b7f673d5
MoYoez <i@himoyo.cn>
2023-04-04 00:09:20 +0800

    Optimize code

--- imgCutter/imgcutter.go
+++ imgCutter/imgcutter.go
@@ -1,43 +1,32 @@
 package imgCutter
 
 import (
-	"golang.org/x/image/draw"
+	"github.com/nfnt/resize"
 	"image"
 )
 
-// Python code with CatmullRom but written in golang.
-func CropImage(img image.Image, width, height int) (destimg image.Image) {
-	sourceWidth := img.Bounds().Dx()
-	sourceHeight := img.Bounds().Dy()
-	sourceAspect := float64(sourceWidth) / float64(sourceHeight)
-	destAspect := float64(width) / float64(height)
-	var rect image.Rectangle
-	if sourceAspect == destAspect {
-		rect = image.Rect(0, 0, width, height)
-	} else if sourceAspect > destAspect {
-		destHeight := int(float64(sourceHeight) * destAspect / sourceAspect)
-		destY := (height - destHeight) / 2
-		rect = image.Rect(0, destY, width, destY+destHeight)
-	} else {
-		destWidth := int(float64(sourceWidth) * sourceAspect / destAspect)
-		destX := (width - destWidth) / 2
-		rect = image.Rect(destX, 0, destX+destWidth, height)
-	}
-	destImg := image.NewRGBA(rect)
-	draw.Draw(destImg, destImg.Bounds(), img, rect.Min, draw.Src)
-	if sourceWidth < width || sourceHeight < height {
-		scale := 1.0
-		if destAspect > sourceAspect {
-			scale = float64(width) / float64(sourceWidth)
+// use resize to do.
+func CropImage(img image.Image, targetWidth, targetHeight int) (destimg image.Image) {
+	bounds := img.Bounds()
+	width := bounds.Dx()
+	height := bounds.Dy()
+	if targetWidth > width || targetHeight > height {
+		var scale float64
+		if float64(targetWidth)/float64(width) > float64(targetHeight)/float64(height) {
+			scale = float64(targetWidth) / float64(width)
 		} else {
-			scale = float64(height) / float64(sourceHeight)
+			scale = float64(targetHeight) / float64(height)
 		}
-		newWidth := int(float64(sourceWidth) * scale)
-		newHeight := int(float64(sourceHeight) * scale)
-		newRect := image.Rect(0, 0, newWidth, newHeight)
-		newImg := image.NewRGBA(newRect)
-		draw.CatmullRom.Scale(newImg, newRect, destImg, destImg.Bounds(), draw.Over, nil)
-		destImg = newImg
+		img = resize.Resize(uint(scale*float64(width)), uint(scale*float64(height)), img, resize.Lanczos3)
+		bounds = img.Bounds()
+		width = bounds.Dx()
+		height = bounds.Dy()
 	}
-	return destImg
+	x := (width - targetWidth) / 2
+	y := (height - targetHeight) / 2
+	rect := image.Rect(x, y, x+targetWidth, y+targetHeight)
+	croppedImg := img.(interface {
+		SubImage(r image.Rectangle) image.Image
+	}).SubImage(rect)
+	return croppedImg
 }
